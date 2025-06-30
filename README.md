319364a353844edbc05fa942b0963fc84ed3b270
William Parker <70612211+Jaspann@users.noreply.github.com>
2025-06-30 11:01:28 -0400

    made test

---
+++ README.md
@@ -0,0 +1,18 @@
+# Portkey Gemini Errors
+
+I made this to record the errors I was getting when using the Portkey Gemini API.
+
+## Setup
+
+Windows 11
+Python 3.12
+uv package manager
+
+## Modifications
+
+This is the default CrewAI project with the following modifications:
+- Added Portkey env variables to the .env file
+- Added three LLM configurations to the src/portkey_test/crew.py file with comments to indicate error file locations
+- Added `prefix.txt` and `no_prefix.txt` to the root folder to record the errors I was getting when using the Portkey Gemini API.
+
+To test, uncomment the LLM configuration you want to use and run `crewai run` from the root folder.
---
+++ src/portkey_test/crew.py
@@ -0,0 +1,102 @@
+from crewai import Agent, Crew, Process, Task, LLM
+from crewai.project import CrewBase, agent, crew, task
+from crewai.agents.agent_builder.base_agent import BaseAgent
+from typing import List
+# If you want to run a snippet of code before or after the crew starts,
+# you can use the @before_kickoff and @after_kickoff decorators
+# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+
+from portkey_ai import createHeaders, PORTKEY_GATEWAY_URL
+import os
+
+# NOT WORKING - prefix.txt
+portkey_llm = LLM(
+    model="gemini/gemini-2.5-flash",
+    base_url=PORTKEY_GATEWAY_URL,
+    api_key="dummy",  # We are using a Virtual key, so this is a placeholder
+    extra_headers=createHeaders(
+        api_key=os.getenv("PORTKEY_API_KEY"),
+        virtual_key=os.getenv("GEMINI_VIRTUAL_API_KEY"),
+    )
+)
+
+# # NOT WORKING - no_prefix.txt
+# portkey_llm = LLM(
+#     model="gemini-2.5-flash",
+#     base_url=PORTKEY_GATEWAY_URL,
+#     api_key="dummy",  # We are using a Virtual key, so this is a placeholder
+#     extra_headers=createHeaders(
+#         api_key=os.getenv("PORTKEY_API_KEY"),
+#         virtual_key=os.getenv("GEMINI_VIRTUAL_API_KEY"),
+#     )
+# )
+
+# # WORKING
+# portkey_llm = LLM(
+#     model="gpt-4o",
+#     base_url=PORTKEY_GATEWAY_URL,
+#     api_key="dummy",  # We are using a Virtual key, so this is a placeholder
+#     extra_headers=createHeaders(
+#         api_key=os.getenv("PORTKEY_API_KEY"),
+#         virtual_key=os.getenv("OPENAI_VIRTUAL_API_KEY"),
+#     )
+# )
+
+@CrewBase
+class PortkeyTest():
+    """PortkeyTest crew"""
+
+    agents: List[BaseAgent]
+    tasks: List[Task]
+
+    # Learn more about YAML configuration files here:
+    # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
+    # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
+    
+    # If you would like to add tools to your agents, you can learn more about it here:
+    # https://docs.crewai.com/concepts/agents#agent-tools
+    @agent
+    def researcher(self) -> Agent:
+        return Agent(
+            config=self.agents_config['researcher'], # type: ignore[index]
+            verbose=True,
+            llm=portkey_llm
+        )
+
+    @agent
+    def reporting_analyst(self) -> Agent:
+        return Agent(
+            config=self.agents_config['reporting_analyst'], # type: ignore[index]
+            verbose=True,
+            llm=portkey_llm
+        )
+
+    # To learn more about structured task outputs,
+    # task dependencies, and task callbacks, check out the documentation:
+    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
+    @task
+    def research_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['research_task'], # type: ignore[index]
+        )
+
+    @task
+    def reporting_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['reporting_task'], # type: ignore[index]
+            output_file='report.md'
+        )
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the PortkeyTest crew"""
+        # To learn how to add knowledge sources to your crew, check out the documentation:
+        # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
+
+        return Crew(
+            agents=self.agents, # Automatically created by the @agent decorator
+            tasks=self.tasks, # Automatically created by the @task decorator
+            process=Process.sequential,
+            verbose=True,
+            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+        )
